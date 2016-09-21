3f06bc168865f337e33a20285aad89853f345aff
David East <deast@google.com>
2016-09-21 06:36:21 -0700

    chore(docs): Formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { componentList } from './components/component-list';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './shared/shared.css']
 })
 export class AppComponent {
   title = 'Angular Material';
